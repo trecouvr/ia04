4a2ab16a63c9a2952317c7c03f40e681cce7d6e8
TitouanR <titouan.rossier@gmail.com>
2013-06-17 15:31:39 +0200

    Remise SImulationConstants OK

--- ia4_project/src/fr/utc/ia04/simulation/SimulationConstants.java
+++ ia4_project/src/fr/utc/ia04/simulation/SimulationConstants.java
@@ -12,20 +12,22 @@ public class SimulationConstants {
 	public final static double	ENV_SIZE =					30;
 	public final static double	ENV_DISCRETIZATION =		1.0;
 	public final static double	ENV_DT =					0.0167;
-	
+
 	// Display
-	public final static double	DISP_SCALE_IMG =				1;
-	public final static double	DISP_SCALE_AGT =				0.6;
+	public final static double	DISP_SCALE_IMG =			1;
+	public final static double	DISP_SCALE_AGT =			0.3;
+	public final static boolean	DISP_SHOW_NAMES =			false;
 
 	// Proportion Agent
 	public final static int		NUM_HUMAN =					100;//3;
 	public final static int		NUM_VAMPIRES =				1;//0;
-	public final static int		NUM_FASTFOOD =				3;//2;
-	public final static int 	NUM_HOTEL =					3;
-	
+	public final static int		NUM_FASTFOOD =				4;//2;
+	public final static int 	NUM_HOTEL =					4;
+
+	// Rates
+	public final static double	RATE_CONVERSION_VAMP =		0.1;
+
 	// Param Agent
-	public final static int		MEM_MIN_EXP =				3;
-	public final static int		MEM_MAX_EXP =				20;
 	public final static double	CHAR_MAX_ENERGY =			1.0;
 	public final static double	CHAR_MAX_AWAKE =			1.0;
 	public final static double	CHAR_MAX_SOCIAL =			1.0;
@@ -35,27 +37,34 @@ public class SimulationConstants {
 	public final static double	METABOLISM_COEF_ENERGY =	1.0/48;
 	public final static double	METABOLISM_COEF_AWAKE =		1.0/72;
 	public final static double	METABOLISM_COEF_SOCIAL =	1.0/200;
-	
+	public static final double	HUMAN_ATTK =				.50; // damages per hours
+	public static final double	VAMPIRE_REGEN =				1.; // damages per hours
+	public static final double	VAMPIRE_ATTAK =				1.; // damages per hours
+	public static final int		HUMAN_MIN_NUMBER_TO_ATTK =	4;
+	public static boolean 		HUMAN_ALWAYSRUN	=			true;
+	public static boolean		HUMAN_SEE_ATTACK =			false;
+	public static boolean		VAMPIRE_ALWAYS_FINISH =		false; // vampire always kill his target even if he doesnt need energy
+
 	// Interactions Dist
 	public final static double	DIST_NEAR =					0.1;
 	public final static double	DIST_INTERACT =				0.4;
 	public final static double	DIST_MEDIUM =				4.0;
 	public final static double	DIST_SECURE =				10.0;
-	
+
 	// Gain
 	public final static double	GAIN_NULL =					0.0;
 	public final static double	GAIN_LOW =					0.2;
 	public final static double	GAIN_MEDIUM =				0.5;
 	public final static double	GAIN_HIGHT =				0.8;
 	public final static double	GAIN_MAX =					1.0;
-	
+
 	// Main Perception Category
 	public final static String	PERC_MAPBORDER =			"mapBorder";
 	public static final String	PERC_VAMPIRE =				"vampire";
 	public static final String	PERC_HUMAN =				"human";
 	public static final String  PERC_FASTFOOD = 			"fastfood";
 	public static final String  PERC_HOTEL = 				"hotel";
-	
+
 	// Main Agent States
 	public final static String	STATE_DONOTHING =			"doNothing";
 	public final static String	STATE_WALKING =				"walk";
@@ -65,9 +74,4 @@ public class SimulationConstants {
 	public static final String  STATE_EATINGHUM =			"eat_hum";
 	public static final String  STATE_ATTAKING =			"attak";
 	public static final String  STATE_RUNNINGAWAY =			"run_away";
-	
-	public static final double	HUMAN_ATTK =				.50;
-	public static final double	VAMPIRE_REGEN =				2.0;
-	public static final double	VAMPIRE_ATTAK =				2.0;
-	public static final int		HUMAN_MIN_NUMBER_TO_ATTK =	5;
 }
